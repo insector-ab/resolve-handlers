1e3ba177ff3f0db7cc47998708c3bd00de769918
Martin Söderholm <matte@insector.se>
2018-01-30 17:05:30 +0100

    Initial commit 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# resolve-handlers
-Resolve handlers from strings
+# Resolve handlers &middot; [![GitHub license](https://img.shields.io/github/license/insector-ab/resolve-handlers.svg)](https://github.com/insector-ab/resolve-handlers/blob/master/LICENSE) [![npm version](https://img.shields.io/npm/v/resolve-handlers.svg?style=flat)](https://www.npmjs.com/package/resolve-handlers)
+
+Resolve handlers using strings. Inspired by event delegation in [Backbone.js](http://backbonejs.org/#Events).
+
+## Installation
+
+```sh
+npm install resolve-handlers
+```
+
+## Example usage
+```javascript
+import {resolveDOMEventHandlers} from './resolve-handlers';
+
+/**
+ * CommentController
+ */
+export default class CommentController {
+
+  constructor(element) {
+    // Target for event listeners
+    this.element = element;
+    // Resolve event handlers
+    this._resolvedEventHandlers = resolveDOMEventHandlers(this.getEventHandlerStrings(), this);
+    // Add event listeners
+    this.addEventListeners();
+  }
+
+  getEventHandlerStrings() {
+    return [
+    	'click .comment > .btn-delete: onDeleteCommentClick',
+    	'focusin .comment: onCommentFocus',
+    	'click .comment: onCommentClick',
+    ];
+  }
+
+  onDeleteCommentClick() {
+  	/* delete handler */
+  }
+
+  onCommentFocus() {
+  	/* focus handler */
+  }
+
+  onCommentClick() {
+  	/* click handler */
+  }
+
+  addEventListeners() {
+    this._resolvedEventHandlers.forEach(eventHandler => {
+      this.element.addEventListener(eventHandler.eventType, eventHandler);
+    });
+  }
+
+  removeEventListeners() {
+    this._resolvedEventHandlers.forEach(eventHandler => {
+      this.element.removeEventListener(eventHandler.eventType, eventHandler);
+    });
+  }
+
+}
+
+```
+
+
+## License
+
+This software is licensed under the [MIT License](https://github.com/insector-ab/resolve-handlers/blob/master/LICENSE).
---
+++ src/index.js
@@ -0,0 +1,171 @@
+import closest from 'component-closest';
+
+/**
+ * Higher Order Function for getting resolvers
+ */
+export function getResolveFunction(getHandler, defaultParseString) {
+  return function(strings, context, parseString) {
+    return strings.map(str => {
+      return getHandler.apply(
+        context,
+        (parseString || defaultParseString)(str)
+      );
+    });
+  };
+}
+
+/**
+ * General getEventHandler
+ */
+export function getEventHandler(handlerName, eventType, ...args) {
+  // Check handler defined
+  if (typeof this[handlerName] === 'undefined') {
+    throw new ReferenceError(`Missing event handler "${handlerName}" for ${this.constructor.name}.`);
+  }
+  // Handler method
+  const handler = (event, ...handlerArgs) => {
+    this[handlerName](event, ...args.concat(handlerArgs));
+  };
+  // Set attributes on handler for easy add/remove listeners
+  handler.eventType = eventType;
+  // Return handler
+  return handler;
+}
+
+/*************************
+ **** Route resolving ****
+ *************************/
+
+/**
+ * Convert route strings to RexExps and handlers.
+ * Example strings:
+ * "^/project/new$: onNewProjectRoute"
+ * "^/user/[\w\-]+$: onUserRoute"
+ */
+export const resolveRoutes = getResolveFunction(getRouteHandler, defaultParseRouteString);
+
+/**
+ * getRouteHandler
+ */
+export function getRouteHandler(handlerName, pathnameRegExp) {
+  // Check handler defined
+  if (typeof this[handlerName] === 'undefined') {
+    throw new ReferenceError(`Missing route handler "${handlerName}" for ${this.constructor.name}.`);
+  }
+  // Handler method
+  const handler = (pathname, ...args) => {
+    const matches = pathname.match(pathnameRegExp);
+    if (matches) {
+      this[handlerName](...matches);
+    }
+  };
+  // Return handler
+  return handler;
+}
+
+/**
+ * Parse route params from string.
+ */
+export function defaultParseRouteString(routeStr) {
+  /* eslint-disable no-unused-vars */
+  const [all, pathnameRegExp, handlerName] = routeStr.match(defaultRouteHandlerRegexp);
+  /* eslint-enable no-unused-vars */
+  return [handlerName, pathnameRegExp];
+}
+
+/**
+ * Route handler string regexp.
+ * Pattern:
+ * "$pathnameRegExp: $handlerName"
+ * Matches strings:
+ * "^/project/new$: onNewProjectRoute"
+ * "^/user/[\w\-]+$: onUserRoute"
+ */
+export const defaultRouteHandlerRegexp = /^(.+):\s(\S+)$/;
+
+/*****************************
+ **** DOM event resolving ****
+ *****************************/
+
+/**
+ * Convert event handler strings to bound event handlers.
+ * Example strings:
+ * "change .component > .selector: onComponentChange"
+ * "click: onClick"
+ */
+export const resolveDOMEventHandlers = getResolveFunction(getDOMEventHandler, defaultParseDOMEventHandlerString);
+
+/**
+ * getDOMEventHandler
+ */
+export function getDOMEventHandler(handlerName, eventType, selector, useCapture, ...args) {
+  // Check handler defined
+  if (typeof this[handlerName] === 'undefined') {
+    throw new ReferenceError(`Missing DOM event handler "${handlerName}" for ${this.constructor.name}.`);
+  }
+  // Handler method
+  const handler = (event, ...handlerArgs) => {
+    if (!selector || !!closest(event.target, selector, event.currentTarget)) {
+      this[handlerName](event, ...args.concat(handlerArgs));
+    }
+  };
+  // Set attributes on handler for easy add/remove listeners
+  handler.eventType = eventType;
+  handler.useCapture = useCapture;
+  // Return handler
+  return handler;
+}
+
+/**
+ * Parse DOM event handler params from string.
+ */
+export function defaultParseDOMEventHandlerString(eventHandlerStr) {
+  /* eslint-disable no-unused-vars */
+  const [all, eventType, selector, handlerName] = eventHandlerStr.match(defaultDOMEventHandlerRegexp);
+  /* eslint-enable no-unused-vars */
+  return [handlerName, eventType, selector, false];
+}
+
+/**
+ * Event handler string regexp.
+ * Pattern:
+ * "$eventType: $handlerName"
+ * "$eventType $selector: $handlerName"
+ * Matches strings:
+ * "click: onClick"
+ * "change .component > .selector: onComponentChange"
+ */
+export const defaultDOMEventHandlerRegexp = /^(\S+)\s*(.*):\s(\S+)$/;
+
+/*******************************
+ **** Model event resolving ****
+ *******************************/
+
+/**
+ * Convert event handler strings to bound event handlers.
+ * Example strings:
+ * "change: onModelChange"
+ * "change width: onWidthChange"
+ */
+export const resolveModelEventHandlers = getResolveFunction(getEventHandler, defaultParseModelEventHandlerString);
+
+/**
+ * Parse model event handler params from string.
+ */
+export function defaultParseModelEventHandlerString(eventHandlerStr) {
+  /* eslint-disable no-unused-vars */
+  const [all, eventType, property, handlerName] = eventHandlerStr.match(defaultDOMEventHandlerRegexp);
+  /* eslint-enable no-unused-vars */
+  return [handlerName, eventType, property];
+}
+
+/**
+ * Event handler string regexp.
+ * Pattern:
+ * "$eventType: $handlerName"
+ * "$eventType $property: $handlerName"
+ * Matches strings:
+ * "change: onModelChange"
+ * "change width: onWidthChange"
+ */
+export const defaultModelEventHandlerRegexp = /^(\S+)\s*(.*):\s(\S+)$/;
